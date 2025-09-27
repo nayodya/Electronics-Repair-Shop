7dbe689fc0864c9eb1113d7169268678716568ba
nayodya <137150827+nayodya@users.noreply.github.com>
2025-09-27 14:51:04 +0530

    backend dockerfile

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3991b27e95f26fe4261218804f521f3bf3c09430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc05998c9d53ed2ced5890a29659de527e70ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
